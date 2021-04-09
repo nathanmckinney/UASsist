74cf1d66b08b6f83db941fcca82713b67f1974ff
Nathan McKinney <mckinney@utk.edu>
2021-04-09 17:04:06 -0400

    init commit

---
+++ docs/usage.md
@@ -0,0 +1,7 @@
+# Usage
+
+To use uassist in a project:
+
+```
+import uassist
+```
---
+++ tests/test_uassist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `uassist` package."""
+
+
+import unittest
+
+from uassist import uassist
+
+
+class TestUassist(unittest.TestCase):
+    """Tests for `uassist` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
