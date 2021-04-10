3bc986ba8497d341bd75ba86df74b961738c8954
Nathan McKinney <mckinney@utk.edu>
2021-04-09 20:01:01 -0400

    import fix

--- uassist/uassist.py
+++ uassist/uassist.py
@@ -1,10 +1,11 @@
 """Main module."""
 import os
 import ipyleaflet
-from ipyleaflet  import Map, FullScreenControl, LayersControl, DrawControl, MeasureControl, ScaleControl, TileLayer
+
+from ipyleaflet import Map, FullScreenControl, LayersControl, DrawControl, MeasureControl, ScaleControl, TileLayer
 
 class Map(ipyleaflet.Map):
     def __init__(self, **kwargs):
         if "center" not in kwargs:
             kwargs["center"] = [40,-100]
-            
+
